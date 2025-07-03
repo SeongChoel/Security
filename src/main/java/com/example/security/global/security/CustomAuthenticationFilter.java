163d8664688b423b268a8a2ea900b311d5ce9245
SeongChoel <tldtlddl1234@gmail.com>
2025-07-03 15:35:40 +0900

    setLogin 필터가 정보를 장난질

--- src/main/java/com/example/security/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/security/global/security/CustomAuthenticationFilter.java
@@ -1,18 +1,27 @@
 package com.example.security.global.security;
 
+import com.example.security.global.Rq;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private final Rq rq;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        rq.setLogin("user1"); //user2가 로그인 했는데 user1로 로그인 처리됨
+
         filterChain.doFilter(request, response); // 다음으로 진행할수있음
 
     }
